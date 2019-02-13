c306ed2e53212e83761d1a31b57bf545e8022160
Ivan26ru <it-1ivandex.ru>
2019-02-13 18:01:48 +0300

    динамичные данные

--- wp/microrazmetka/wpsidebar.php
+++ wp/microrazmetka/wpsidebar.php
@@ -1,7 +1,5 @@
 <div style="display: none;" itemscope itemtype="http://schema.org/WPSideBar">
-	<meta itemprop="name" content="<?php echo seo_info('title',$post->ID); ?>"/>
-    <meta itemprop="description" content="<?php echo seo_info('desc',$post->ID); ?>"/>
-    <meta itemprop="keywords" content="<?php echo seo_info('key',$post->ID); ?>"/>
-    <meta itemprop="copyrightYear" content="2016"/>
-    <meta itemprop="copyrightHolder" content="<?php echo get_site_url(); ?>"/>
+<meta itemprop="name" content="<?php echo seo_info('title',$post->ID); ?>"/>
+<meta itemprop="description" content="<?php echo seo_info('desc',$post->ID); ?>"/>
+<meta itemprop="keywords" content="<?php echo seo_info('key',$post->ID); ?>"/>
 </div>
