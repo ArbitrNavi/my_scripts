1e649471cc50a14d6ca040437064d4cd760afb04
Ivan26ru <it-1ivandex.ru>
2019-02-13 18:28:33 +0300

    Добавил адрес сайта динамический

--- wp/microrazmetka/wpsidebar.php
+++ wp/microrazmetka/wpsidebar.php
@@ -1,5 +1,7 @@
 <div style="display: none;" itemscope itemtype="http://schema.org/WPSideBar">
-<meta itemprop="name" content="<?php echo seo_info('title',$post->ID); ?>"/>
-<meta itemprop="description" content="<?php echo seo_info('desc',$post->ID); ?>"/>
-<meta itemprop="keywords" content="<?php echo seo_info('key',$post->ID); ?>"/>
+	<meta itemprop="name" content="<?php echo seo_info('title',$post->ID); ?>"/>
+    <meta itemprop="description" content="<?php echo seo_info('desc',$post->ID); ?>"/>
+    <meta itemprop="keywords" content="<?php echo seo_info('key',$post->ID); ?>"/>
+    <meta itemprop="copyrightYear" content="2016"/>
+    <meta itemprop="copyrightHolder" content="<?php echo get_site_url(); ?>"/>
 </div>
