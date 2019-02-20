007aa87200c9a24584f8d6a0843c1c7e6444ef66
Ivan26ru <it-1ivandex.ru>
2019-02-20 06:58:20 +0300

    id категории

---
+++ wp/id_cat.php
@@ -0,0 +1,11 @@
+<?php 
+// id текущей категории
+    $queried_object = get_queried_object();
+    $taxonomy = $queried_object->taxonomy;
+    $term_id = $queried_object->term_id;
+
+// id категории к которой принадлежит текущая запись
+    $infocat = get_the_category();
+    $info = $infocat[0]->cat_ID;
+    var_dump($info);
+ ?>
