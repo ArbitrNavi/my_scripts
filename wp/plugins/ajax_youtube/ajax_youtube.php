c5a3d828b617d7c98981f7074037f604dea5d527
Ivan26ru <it-1ivan@yandex.ru>
2019-03-12 12:07:55 +0300

    плагин аякс подкгрузки

---
+++ wp/plugins/ajax_youtube/ajax_youtube.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Ajax подгужка видео роликов
+ * Description: Позволяет загружать ролик только после нажатия на на него, изначально грузиться только изображения этого ролика, что существенно снижает нагрузку при загрузке страницы.
+ * Plugin URI:  https://vk.com/ivan26ru
+ * Author URI:  https://vk.com/ivan26ru
+ * Author:      Иван	
+ * Version:     1.0
+ *
+ * Text Domain: Идентификатор перевода, указывается в load_plugin_textdomain()
+ * Domain Path: Путь до файла перевода. Нужен если файл перевода находится не в той же папке, в которой находится текущий файл.
+ *              Например, .mo файл находится в папке myplugin/languages, а файл плагина в myplugin/myplugin.php, тогда тут указываем "/languages"
+ *
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Network:     Укажите "true" для возможности активировать плагин по все сети сайтов (для Мультисайтовой сборки).
+ */
