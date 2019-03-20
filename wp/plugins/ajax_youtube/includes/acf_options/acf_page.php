0a8584d1dbd5fc2d48964b835abd927ee6f4206e
Ivan26ru <it-1ivan@yandex.ru>
2019-03-20 20:27:06 +0300

    добавил отдельную страницу настроек, что б не было сбоев

--- wp/plugins/ajax_youtube/includes/acf_options/acf_page.php
+++ wp/plugins/ajax_youtube/includes/acf_options/acf_page.php
@@ -2,9 +2,9 @@
 // Добавление страницы настроек в тему сайта wordpress с установленным плагином acf pro
 if( function_exists('acf_add_options_page') ) {
 	acf_add_options_page(array(
-	'page_title' => 'Дополнительные настройки',
-	'menu_title' => 'Дополнительные настройки',
-	'menu_slug' => 'theme-general-settings',
+	'page_title' => 'Шаблонные шорткоды видео',
+	'menu_title' => 'Шаблонные шорткоды видео',
+	'menu_slug' => 'custom_setting',
 	'capability' => 'edit_posts',
 	// 'redirect' => false
 	));
@@ -12,7 +12,7 @@ if( function_exists('acf_add_options_page') ) {
 	'page_title' => 'Шаблонные шорткоды видео',
 	'menu_title' => 'Шаблонные шорткоды видео',
 	'menu_slug' => 'shortkod_shablon',
-	'parent_slug' => 'theme-general-settings',
+	'parent_slug' => 'custom_setting',
 	));
 
 };
