faf40c84e2e7727818f002c1c465517f97b1ecab
Ivan26ru <it-1ivandex.ru>
2019-03-28 09:13:54 +0300

    исправил ошибки, сделал динамичным для любых сайтов

--- wp/schema.org/microrazmetka/website.php
+++ wp/schema.org/microrazmetka/website.php
@@ -1,1 +1,12 @@
-<script type='application/ld+json'>{"@context":"<?php echo protocol();?>:\/\/schema.org","@type":"WebSite","@id":"#website","url":"<?php echo protocol();?>:\/\/tamara.ru\/","name":"Тамара - рекламное агенство полного цикла","potentialAction":{"@type":"SearchAction","target":"<?php echo protocol();?>:\/\/tamara.ru\/?s={search_term_string}","query-input":"required name=search_term_string"}}</script>
+<script type="application/ld+json">
+   {
+      "@context": "http://schema.org",
+      "@type": "WebSite",
+      "url": "<?php echo get_site_url(); ?>/",
+        "potentialAction": {
+        "@type": "SearchAction",
+        "target": "<?php echo get_site_url(); ?>/?s={query}",
+        "query-input": "required name=query"
+        }
+      }
+</script>
