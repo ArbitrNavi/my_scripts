ec82a07571ef90c01d5791dd75b25054a438ffae
Ivan26ru <it-1ivan@yandex.ru>
2019-04-10 15:36:28 +0300

    добавлены комментарии

--- wp/URL_404_GET.php
+++ wp/URL_404_GET.php
@@ -1,11 +1,11 @@
 <?php 
 // отдает 404 ответ на url типа: 
-// https://site.ru/?p=123
-// https://site.ru/?С=Hiu23oiuhUO
+// https://site.ru/?p=123 //урлы записей по ID
+// https://site.ru/?С=Hiu23oiuhUO //какая то шляпа непонятная
 if(isset($_GET['p']) || isset($_GET['C'])) 
 { 
     header("HTTP/1.0 404 Not Found"); 
     header("HTTP/1.1 404 Not Found"); 
     include '404.php'; 
     exit(); 
-}
+};
