13b26016544f6140f507267f46dac51b3138e80b
Ivan26ru <it-1ivandex.ru>
2019-04-10 09:34:36 +0300

    переименовал файл и добавил еще условие с GET запросом

--- wp/URL_404_p=.php
+++
@@ -1,9 +0,0 @@
-<?php 
-// отдает 404 ответ на url типа https://site.ru/?p=123
-if(isset($_GET['p'])) 
-{ 
-    header("HTTP/1.0 404 Not Found"); 
-    header("HTTP/1.1 404 Not Found"); 
-    include '404.php'; 
-    exit(); 
-} ?>
