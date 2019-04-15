1b8ed8beb40a6f37cb4381471108bc335302d858
Ivan26ru <it-1ivandex.ru>
2019-04-15 06:41:00 +0300

    очистил код от мусора и добавил нужные фишки

--- wp/this_my_theme/functions.php
+++ wp/this_my_theme/functions.php
@@ -97,7 +97,6 @@ if (!function_exists('add_scripts')) { // если ф-я уже есть в до
 	    if(is_admin()) return false; // если мы в админке - ничего не делаем
 	    wp_deregister_script('jquery'); // выключаем стандартный jquery
 	    wp_enqueue_script('jquery','//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js','','',true); // добавляем свой
-	    wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.min.js','','',true); // бутстрап
 	    wp_enqueue_script('main', get_template_directory_uri().'/js/main.js','','',true); // и скрипты шаблона
 	}
 }
@@ -106,55 +105,13 @@ add_action('wp_print_styles', 'add_styles'); // приклеем ф-ю на до
 if (!function_exists('add_styles')) { // если ф-я уже есть в дочерней теме - нам не надо её определять
 	function add_styles() { // добавление стилей
 	    if(is_admin()) return false; // если мы в админке - ничего не делаем
-	    wp_enqueue_style( 'bs', get_template_directory_uri().'/css/bootstrap.min.css' ); // бутстрап
-		wp_enqueue_style( 'main', get_template_directory_uri().'/style.css' ); // основные стили шаблона
+		wp_enqueue_style( 'main_reset', get_template_directory_uri().'/css/reset.css' ); 
+		wp_enqueue_style( 'main_fonts', get_template_directory_uri().'/css/fonts.css' ); 
+		wp_enqueue_style( 'main_style_wp', get_template_directory_uri().'/css/style_wp.css' ); 
+		wp_enqueue_style( 'main', get_template_directory_uri().'/style.css' ); 
+		wp_enqueue_style( 'main_media', get_template_directory_uri().'/css/media.css' ); 
 	}
 }
 
-if (!class_exists('bootstrap_menu')) {
-	class bootstrap_menu extends Walker_Nav_Menu { // внутри вывод 
-		private $open_submenu_on_hover; // параметр который будет определять раскрывать субменю при наведении или оставить по клику как в стандартном бутстрапе
-
-		function __construct($open_submenu_on_hover = true) { // в конструкторе
-	        $this->open_submenu_on_hover = $open_submenu_on_hover; // запишем параметр раскрывания субменю
-	    }
-
-		function start_lvl(&$output, $depth = 0, $args = array()) { // старт вывода подменюшек
-			$output .= "\n<ul class=\"dropdown-menu\">\n"; // ул с классом
-		}
-		function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) { // старт вывода элементов
-			$item_html = ''; // то что будет добавлять
-			parent::start_el($item_html, $item, $depth, $args); // вызываем стандартный метод родителя
-			if ( $item->is_dropdown && $depth === 0 ) { // если элемент содержит подменю и это элемент первого уровня
-			   if (!$this->open_submenu_on_hover) $item_html = str_replace('<a', '<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"', $item_html); // если подменю не будет раскрывать при наведении надо добавить стандартные атрибуты бутстрапа для раскрытия по клику
-			   $item_html = str_replace('</a>', ' <b class="caret"></b></a>', $item_html); // ну это стрелочка вниз
-			}
-			$output .= $item_html; // приклеиваем теперь
-		}
-		function display_element($element, &$children_elements, $max_depth, $depth = 0, $args, &$output) { // вывод элемента
-			if ( $element->current ) $element->classes[] = 'active'; // если элемент активный надо добавить бутстрап класс для подсветки
-			$element->is_dropdown = !empty( $children_elements[$element->ID] ); // если у элемента подменю
-			if ( $element->is_dropdown ) { // если да
-			    if ( $depth === 0 ) { // если li содержит субменю 1 уровня
-			        $element->classes[] = 'dropdown'; // то добавим этот класс
-			        if ($this->open_submenu_on_hover) $element->classes[] = 'show-on-hover'; // если нужно показывать субменю по хуверу
-			    } elseif ( $depth === 1 ) { // если li содержит субменю 2 уровня
-			        $element->classes[] = 'dropdown-submenu'; // то добавим этот класс, стандартный бутстрап не поддерживает подменю больше 2 уровня по этому эту ситуацию надо будет разрешать отдельно
-			    }
-			}
-			parent::display_element($element, $children_elements, $max_depth, $depth, $args, $output); // вызываем стандартный метод родителя
-		}
-	}
-}
-
-if (!function_exists('content_class_by_sidebar')) { // если ф-я уже есть в дочерней теме - нам не надо её определять
-	function content_class_by_sidebar() { // функция для вывода класса в зависимости от существования виджетов в сайдбаре
-		if (is_active_sidebar( 'sidebar' )) { // если есть
-			echo 'col-sm-9'; // пишем класс на 80% ширины
-		} else { // если нет
-			echo 'col-sm-12'; // контент на всю ширину
-		}
-	}
-}
 
 ?>
--- wp/this_my_theme/js/main.js
+++ wp/this_my_theme/js/main.js
@@ -1,1 +1,7 @@
-// custom scripts
+console.log('скрипт подключен'); //проверка подключения скрипта
+jQuery(document).ready(function($) { //ожидание полной загрузки дом дерева и возвожность работы со знаком доллара, в движках
+    console.log($); //проверка работоспособности JQuery
+    console.log(jQuery.fn.jquery);//узнать версию JQuery
+
+
+}); //конец ready// custom scripts
