cb083cc23050718c92ca98d2284af4bc4ed6ddba
Ivan26ru <it-1ivan@yandex.ru>
2019-04-26 13:39:15 +0300

    антиспам для CF7

---
+++ wp/antispam_cf7.php
@@ -0,0 +1,16 @@
+<!-- статья где взял информацию http://reset.name/wordpress-ru/contact-form-7-antispam-bez-kapchi/ -->
+
+<script>
+	// антиспам cf7
+	// имитируем клик
+jQuery('.agree').click();
+
+</script>
+
+<style>
+/*делаем невидимым для человека*/
+	.agree {display:none}
+</style>
+
+<!-- вставить в шаблокн формы CF7 -->
+[acceptance agree class:agree default:on invert]
