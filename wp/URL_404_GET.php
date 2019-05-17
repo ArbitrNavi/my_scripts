718446379fcaf39fa3daa12512aced5ea60a640d
Ivan26ru <it-1ivandex.ru>
2019-05-17 05:06:34 +0300

    использовал более подходящую функцию

--- wp/URL_404_GET.php
+++ wp/URL_404_GET.php
@@ -2,10 +2,10 @@
 // отдает 404 ответ на url типа: 
 // https://site.ru/?p=123 //урлы записей по ID
 // https://site.ru/?С=Hiu23oiuhUO //какая то шляпа непонятная
+// можно использовать URI страницы, кгда нужно отдавать 404 на ненужные страницы
+// $_SERVER['REQUEST_URI']=== '/elementor-popup/uznat-stoimost/'
 if(isset($_GET['p']) || isset($_GET['C'])) 
 { 
-    header("HTTP/1.0 404 Not Found"); 
-    header("HTTP/1.1 404 Not Found"); 
-    include '404.php'; 
+	wp_redirect( home_url( '/404/' ) );
     exit(); 
 };
