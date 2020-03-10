217c0ddcea45e9961fba4329e0b4d7b1c78100e3
Ivan26ru <it-1ivan@yandex.ru>
2020-03-10 11:55:24 +0300

    alert yes and no

---
+++ js/srcipt/alert_yes_no.js
@@ -0,0 +1,11 @@
+// Вывод алерта да или нет
+if (confirm("Choose Yes or Not"))
+  {
+  alert("You pressed OK!");
+  //тут удаление записи из базы
+  }
+else
+  {
+  alert("You pressed Cancel!");
+  //тут просто остаемся на странице
+}
