ca014876b585174f3f73b79f8926aa933f9c2dce
Ivan26ru <it-1ivan@ya.ru>
2020-04-08 12:36:38 +0300

    добавил версию к стилям, вдруг где то будет кеш подтягиваться

--- wp/plugins/ajax_youtube/includes/add_style_scripts.php
+++ wp/plugins/ajax_youtube/includes/add_style_scripts.php
@@ -3,7 +3,7 @@
 add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
 // add_action('wp_print_styles', 'theme_name_scripts'); // можно использовать этот хук он более поздний
 function theme_name_scripts() {
-	wp_enqueue_style( 'css_youtube', plugins_url('ajax_youtube') . '/assets/css/gallery.css' );
+	wp_enqueue_style( 'css_youtube', plugins_url('ajax_youtube') . '/assets/css/gallery.css?2' );
 	wp_enqueue_script( 'youtube_gallery', plugins_url('ajax_youtube') . '/assets/js/youtube_gallery.js', array(), '1.0.0', true );
 	wp_enqueue_script( 'gm_lazy', plugins_url('ajax_youtube') . '/assets/js/gm_lazy.js', array(), '1.0.0', true );
 }
