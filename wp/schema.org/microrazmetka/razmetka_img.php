f41ba64b786f06579c8106c52ccc3153b809ea6d
Ivan26ru <it-1ivan@ya.ru>
2020-05-06 05:06:12 +0300

    изменил встраиваемый модуль в плагин

--- wp/schema.org/microrazmetka/razmetka_img.php
+++
@@ -1,9 +0,0 @@
-<?php
-function mayak_filter_image($content) {
-$ar_mk	= '!<img (.*?) width="(.*?)" height="(.*?)" (.*?)/>!si';
-$br_mk = '<span itemprop="image" itemscope itemtype="<?php echo protocol();?>://schema.org/ImageObject"><img itemprop="url contentUrl" \\1 width="\\2" height="\\3" \\4/><meta itemprop="width" content="\\2"><meta itemprop="height" content="\\3"></span>';
-$content = preg_replace($ar_mk, $br_mk, $content);
-	return $content;
-}
-add_filter('the_content', 'mayak_filter_image');
- ?>
