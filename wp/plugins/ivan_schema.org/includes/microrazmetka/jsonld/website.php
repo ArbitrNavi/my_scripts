15b9bf6b3900d689ee05fce6a42f4a0f5e6cb69c
Ivan26ru <it-1ivan@ya.ru>
2020-08-07 08:10:17 +0300

    добавление разметки через jsonld

--- wp/plugins/ivan_schema.org/includes/microrazmetka/jsonld/website.php
+++ wp/plugins/ivan_schema.org/includes/microrazmetka/jsonld/website.php
@@ -2,9 +2,9 @@
    {
       "@context": "http://schema.org",
       "@type": "WebSite",
-      "name": <?php echo get_bloginfo('name'); ?>
+      "name": "<?php echo get_bloginfo('name'); ?>",
       "url": "<?php echo get_site_url(); ?>/",
-        "potentialAction": {
+      "potentialAction": {
         "@type": "SearchAction",
         "target": "<?php echo get_site_url(); ?>/?s={query}",
         "query-input": "required name=query"
