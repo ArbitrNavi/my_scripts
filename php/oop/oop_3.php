229acc27cc651542cccec205f83026c7bf25ad5d
Ivan <it-1ivan@ya.ru>
2021-02-09 16:16:59 +0300

    ООП 6 protected

--- php/oop/oop_3.php
+++ php/oop/oop_3.php
@@ -1,10 +1,16 @@
 <?php
 //Наследование - позволяет строить дерево, кластер из объектов. Все что есть в текущем объекте будет доступно в унаследованном
+//Родительские и дочерние классы
+
+//protected (защищенный) - доступен для всех объектов находящихся в дереве наследования данного класса данного объекта
+//public - открыть для всех объектов
+//private - скрыт для всех объектов
 
 
 class Animal{
-    public $name;
+    protected $name;
     public $color;
+    public $age;
 
     public function makeSound(){
         //
@@ -16,9 +22,15 @@ class Animal{
 
 }
 
+//дочерний элемент класса Cat
 class Cat extends Animal{
 
+    public function move()
+    {
+        //реализация #2
+    }
 }
 
 $myCat = new Cat;
-
+$myCat->name;
+$myCat->move(); //реализация #2
