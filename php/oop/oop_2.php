a95cc0581f77fc2a976a53c4ac6cf4cd4385e046
Ivan <it-1ivan@ya.ru>
2021-02-09 15:16:24 +0300

    ООП 5 приватные аттрибуты

---
+++ php/oop/oop_2.php
@@ -0,0 +1,46 @@
+<?php
+//ООП 5 приветные аттрибуты
+
+
+// Объект - это сущность, одновременно содержащая данные и поведения
+//объект - определяется 2 компонентами:
+//Аттрибутами(размер, цвет, скорость, ...)
+//Поведениями(бегать, прыгать, читать, ...)
+
+class Car
+{
+    public $color;
+    public $year;
+    public $manufacturer;
+
+//    действия / методы к объекту
+
+    public function __construct($color, $year, $izgotovitel)
+    {
+        $this->color = $color;
+        $this->year = $year;
+        $this->year = $izgotovitel;
+    }
+
+    public function startEngine()
+    {
+    //    большой кусок сода завода мотора
+    }
+
+    public function changeColor($color)
+    {
+        $this->color = $color;
+    }
+
+    public function go()
+    {
+        //    реализация езды
+    }
+
+//действие КОНСТРУКТОР - выполняется без явного вызова
+}
+
+$myCar = new Car('red',2017,'Mersees'); // конкретный объект - создать новый экземпляр машины на основе чертежа Car. Теперь это экземляр машины
+
+
+
