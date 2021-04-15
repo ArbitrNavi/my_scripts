b6c43be5be0b716791e5ade8636cf297efd596b7
Ivan <it-1ivan@ya.ru>
2021-04-15 16:40:32 +0300

    добавил вывод сайта в логах

--- server/del_virus_ico.sh
+++ server/del_virus_ico.sh
@@ -1,12 +1,17 @@
 #!/bin/bach
-DIR_WWW="/var/www"
-#DIR_SITE="$DIR_WWW/forza.ba"
-DIR_SITE="$DIR_WWW/forza.mk"
-#DIR_SITE="$DIR_WWW/kreddy.ba"
-#DIR_SITE="$DIR_WWW/kreddy.mk"
-echo `date` >> $DIR_SITE/log.virus.txt
-grep -rl "^@include.*;$" $DIR_SITE/ >> $DIR_SITE/log.virus.txt
-grep -rl "^@include.*;$" $DIR_SITE/ | xargs sed -i "s/^@include.*;$/ /g"
-find $DIR_SITE/ -name *.ico  >> $DIR_SITE/log.virus.txt
-find $DIR_SITE/ -name *.ico  | xargs rm -f
+#var DIR_SITE - site folder
 
+#DIR_SITE="forza.ba"
+DIR_SITE="forza.mk"
+#DIR_SITE="kreddy.ba"
+#DIR_SITE="kreddy.mk"
+
+DIR_ALL="/var/www/$DIR_SITE"
+
+echo "---" >> $DIR_ALL/log.virus.txt
+echo `date` >> $DIR_ALL/log.virus.txt
+echo "$DIR_SITE" >> $DIR_ALL/log.virus.txt
+grep -rl "^@include.*;$" $DIR_ALL/ >> $DIR_ALL/log.virus.txt
+grep -rl "^@include.*;$" $DIR_ALL/ | xargs sed -i "s/^@include.*;$/ /g"
+find $DIR_ALL/ -name *.ico  >> $DIR_ALL/log.virus.txt
+find $DIR_ALL/ -name *.ico  | xargs rm -f
