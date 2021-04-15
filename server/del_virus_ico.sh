f1ba959be4ade634bbc45ab26f836e00b0521ce6
Ivan <it-1ivan@ya.ru>
2021-04-15 16:24:30 +0300

    fix error

--- server/del_virus_ico.sh
+++ server/del_virus_ico.sh
@@ -4,9 +4,9 @@ DIR_WWW="/var/www"
 DIR_SITE="$DIR_WWW/forza.mk"
 #DIR_SITE="$DIR_WWW/kreddy.ba"
 #DIR_SITE="$DIR_WWW/kreddy.mk"
-echo `date` >> $DIR_DIR_SITE/log.virus.txt
-grep -rl "^@include.*;$" $DIR_DIR_SITE/ >> $DIR_DIR_SITE/log.virus.txt
-grep -rl "^@include.*;$" $DIR_DIR_SITE/ | xargs sed -i "s/^@include.*;$/ /g"
-find $DIR_DIR_SITE/ -name *.ico  >> $DIR_DIR_SITE/log.virus.txt
-find $DIR_DIR_SITE/ -name *.ico  | xargs rm -f
+echo `date` >> $DIR_SITE/log.virus.txt
+grep -rl "^@include.*;$" $DIR_SITE/ >> $DIR_SITE/log.virus.txt
+grep -rl "^@include.*;$" $DIR_SITE/ | xargs sed -i "s/^@include.*;$/ /g"
+find $DIR_SITE/ -name *.ico  >> $DIR_SITE/log.virus.txt
+find $DIR_SITE/ -name *.ico  | xargs rm -f
 
