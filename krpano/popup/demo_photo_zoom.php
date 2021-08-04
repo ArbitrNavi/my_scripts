bd820c1a4656b97fa0240e0f04a4a5bd9afa51b1
i.pisarenko <i.pisarenko@crmguru.ru>
2021-08-04 14:59:16 +0300

    add demo popup

---
+++ krpano/popup/demo_photo_zoom.php
@@ -0,0 +1,9 @@
+<?php include ('header.php'); ?>
+
+    <div class="content">
+        <div class="popup_img">
+            <img src="img/1.jpg">
+        </div>
+    </div>
+
+<?php include ('footer.php'); ?>
