218bcbec2c7db17b6d8a178250d20cb242f5926b
i.pisarenko <i.pisarenko@crmguru.ru>
2021-08-09 07:51:20 +0300

    fix img center

--- krpano/popup/demo_photo_zoom.php
+++ krpano/popup/demo_photo_zoom.php
@@ -1,6 +1,6 @@
 <?php include ('header.php'); ?>
 
-    <div class="content">
+    <div class="content content_popup_img">
         <div class="popup_img">
             <img src="<?php echo dirname($_SERVER['PHP_SELF']) ?>/img/1.jpg">
         </div>
