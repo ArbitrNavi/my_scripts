0214b16eea7e68755746ee7d537dbeb51de4e6c7
i.pisarenko <i.pisarenko@crmguru.ru>
2021-08-12 09:00:47 +0300

    add size img

--- wp/plugins/ivan_schema.org/includes/microrazmetka/jsonld/ImageObject.php
+++ wp/plugins/ivan_schema.org/includes/microrazmetka/jsonld/ImageObject.php
@@ -4,6 +4,8 @@
   "@type": "ImageObject",
   "name": "<?php echo seo_info('title',$post_id); ?>",
   "contentUrl": "<?php echo $arr_thumbnail['src']; ?>",
+  "width": "<?php echo $arr_thumbnail['width']; ?>",
+  "height": "<?php echo $arr_thumbnail['height']; ?>",
   "description": "<?php echo seo_info('desc',$post_id); ?>",
 }
 </script>
