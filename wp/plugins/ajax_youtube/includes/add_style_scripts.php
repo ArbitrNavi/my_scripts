f43cba0f0ad95f0f16c5b8dfee42b3cc85932288
i.pisarenko <i.pisarenko@crmguru.ru>
2021-08-29 00:04:14 +0300

    add new modal library and update version files

--- wp/plugins/ajax_youtube/includes/add_style_scripts.php
+++ wp/plugins/ajax_youtube/includes/add_style_scripts.php
@@ -1,10 +1,17 @@
-<?php 
-// правильный способ подключить стили и скрипты
-add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
-// add_action('wp_print_styles', 'theme_name_scripts'); // можно использовать этот хук он более поздний
-function theme_name_scripts() {
+<?php
+
+function add_jquery_and_my_scripts(){
+	wp_enqueue_script('jquery');
 	wp_enqueue_style( 'css_youtube', plugins_url('ajax_youtube') . '/assets/css/gallery.css?2' );
-	wp_enqueue_script( 'youtube_gallery', plugins_url('ajax_youtube') . '/assets/js/youtube_gallery.js', array(), '1.0.0', true );
+	wp_enqueue_script( 'youtube_gallery', plugins_url('ajax_youtube') . '/assets/js/youtube_gallery.js?2', array(), '1.0.0', true );
 	wp_enqueue_script( 'gm_lazy', plugins_url('ajax_youtube') . '/assets/js/gm_lazy.js', array(), '1.0.0', true );
+
+	//	modal
+	wp_enqueue_style( 'jquery.modal.css', plugins_url('ajax_youtube') . '/assets/lib/modal/jquery.modal.css' );
+	wp_enqueue_style( 'custom_modal.css', plugins_url('ajax_youtube') . '/assets/lib/modal/custom_modal.css' );
+	wp_enqueue_script( 'jquery.modal.min.js', plugins_url('ajax_youtube') . '/assets/lib/modal/jquery.modal.min.js', array(), '1.0.0', true );
+
 }
+add_action( 'wp_footer', 'add_jquery_and_my_scripts' );
+
  ?>
