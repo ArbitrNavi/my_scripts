f61757e2a828cf6966be418be6487d8a55521b17
i.pisarenko <i.pisarenko@crmguru.ru>
2021-09-01 06:58:15 +0300

    update v3.2 rename class is .modal to .ay_modal

--- wp/plugins/ajax_youtube/includes/modal.php
+++ wp/plugins/ajax_youtube/includes/modal.php
@@ -2,7 +2,7 @@
 // выполняем функцию перед wp_footer
 add_action( 'wp_footer', 'footer_modal' );
 function footer_modal(){
-	echo '<div id="ajax_youtube_modal" class="ajax_youtube_modal modal" >
+	echo '<div id="ajax_youtube_modal" class="ajax_youtube_modal ay_modal" >
 </div>';
 //	<iframe id="ajax_youtube_iframe" src="" style="width: 100%; height: 100%;"></iframe>
 }
