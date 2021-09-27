42baee14cbbab31263573a80726c507ca67837d3
i.pisarenko <i.pisarenko@crmguru.ru>
2021-09-27 10:12:57 +0300

    fix error

--- js/srcipt/banner_ads_one_day.js
+++ js/srcipt/banner_ads_one_day.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function($) {
 
 // рекламный банер
 // Баннер должен прятаться с Fade-out эффектом при нажатии на .banner-close
@@ -10,40 +10,40 @@ jQuery(document).ready(function($){
 // Если пользователь закрыл баннер, он не должен ему отображаться в течении дня (24 часов), на следующий день снова появляться
 
 
-
-ifclick=true;//было ли нажатие
-Data = new Date();//текущая дата
-Day = Data.getDate();//текущий день
+	ifclick = true;//было ли нажатие
+	Data = new Date();//текущая дата
+	Day = Data.getDate();//текущий день
 
 // условие отвечал раньше или нет
 // есть ли данные в сессии
-if (!sessionStorage.getItem('banner_front_page')) {
+	if (!sessionStorage.getItem('banner_front_page')) {
 
 // console.log(sessionStorage.getItem('banner_front_page'));
 // console.log(ifclick);
 
 
 // close
-	$('.banner-close').click(function(){//клик по кнопке закрытия
-		$('.homepage-banner').fadeOut();//скрыть модальные блок
-		// console.log('click')
-		sessionStorage.setItem('banner_front_page', true);//добавить данные в сессию
-		ifclick=false;//кнопка нажалась
+		$('.banner-close').click(function () {//клик по кнопке закрытия
+			$('.homepage-banner').fadeOut();//скрыть модальные блок
+			// console.log('click')
+			sessionStorage.setItem('banner_front_page', true);//добавить данные в сессию
+			ifclick = false;//кнопка нажалась
 
-		localStorage.setItem('banner_front_page_data', Day);//добавить данные в локал стораж(не стирается после переоткрытия браузера)
-		// localStorage.setItem('banner_front_page_data', '22');
+			localStorage.setItem('banner_front_page_data', Day);//добавить данные в локал стораж(не стирается после переоткрытия браузера)
+			// localStorage.setItem('banner_front_page_data', '22');
 
-	});//.клик по кнопке закрытия
+		});//.клик по кнопке закрытия
 
 // scroll
-	    $(window).scroll(function(){//прокручивание
-	        if(
-	        	$(this).scrollTop()>400//прокрутка экрана
-	        	&&ifclick//было ли закрыто ранее
-	        	&&!sessionStorage.getItem('banner_front_page')//если нет записи в сессии
-	        	||localStorage.getItem('banner_front_page_data')<Day// ИЛИ есть запись в локал сторадж меньше чем текущий день
-	        ){//условие при котором отображается модальный блок
-	            $('.homepage-banner').fadeIn();//отображение модального блоко
-	        };
-	    });//.прокручивание
-};
+		$(window).scroll(function () {//прокручивание
+			if (
+				$(this).scrollTop() > 400//прокрутка экрана
+				&& ifclick//было ли закрыто ранее
+				&& !sessionStorage.getItem('banner_front_page')//если нет записи в сессии
+				|| localStorage.getItem('banner_front_page_data') < Day// ИЛИ есть запись в локал сторадж меньше чем текущий день
+			) {//условие при котором отображается модальный блок
+				$('.homepage-banner').fadeIn();//отображение модального блоко
+			}
+		});//.прокручивание
+	}
+});
