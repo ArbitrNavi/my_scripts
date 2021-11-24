a241f747b61e3b1eeedc24f70af91b80405b2181
i.pisarenko <i.pisarenko@crmguru.ru>
2021-11-24 07:19:25 +0300

    Проверка на существование (объявление, определена) переменной

---
+++ js/srcipt/var_is_not_defined.js
@@ -0,0 +1,6 @@
+//Проверка на существование (объявление, определена) переменной
+typeof ekonomika_one_project !== 'undefined'
+
+if (typeof variable !== 'undefined') {
+    // переменная существует
+}
