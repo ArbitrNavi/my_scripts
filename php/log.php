a6442efb0a4cc01e4daceb2c1be30e44ca361e76
i.pisarenko <i.pisarenko@crmguru.ru>
2021-12-06 11:51:37 +0300

    add chmod

--- php/log.php
+++ php/log.php
@@ -9,13 +9,20 @@ $data_GET = '$_GET: ' . print_r($_GET, true).PHP_EOL;
 
 $data_all = $razdelitel . $data_REQUEST . $data_POST . $data_GET;
 
-if (!is_dir('log')){
-    mkdir('log','777');
-}
+//создание каталогов, не везде срабатывает как надо, поэтому отключаем
+//if (!is_dir(__DIR__ .'/log')){
+//    mkdir(__DIR__ .'/log','655');
+//}
 
 //помесячное создание
-$name_txt = 'log/log_' . date('m.Y'); //12.2021
+$name_txt = __DIR__ . '/log_' . date('m.Y') . '.txt'; //log_12.2021.txt
+
+
+//изменяем права, что б нельзя было читать извне
+$chmod = '0244';
+chmod($name_txt, $chmod);  // восьмеричное, верный способ
 
 //Запись в файл
 file_put_contents($name_txt, $data_all, FILE_APPEND);
+//var_dump($name_txt, $chmod);
 ?>
