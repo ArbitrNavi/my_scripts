4ceee2adfd3955599331dd94f0e9a2363a3f4501
i.pisarenko <i.pisarenko@crmguru.ru>
2021-12-20 10:37:35 +0300

    Выход из аккаунта по ссылке без подтверждения

---
+++ wp/exit_account.php
@@ -0,0 +1,3 @@
+<!-- Выход из аккаунта по ссылке без подтверждения -->
+<!-- https://wp-kama.ru/function/wp_logout_url -->
+<a href="<?php echo wp_logout_url( home_url() ); ?>" title="Выход">Выход</a>
