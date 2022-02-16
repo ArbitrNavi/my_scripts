92498642f38f4c83d0595da7a4644d7bbb4978ba
i.pisarenko <i.pisarenko@crmguru.ru>
2022-02-16 08:36:08 +0300

    move to assets

--- wp/this_my_theme/functions.php
+++ wp/this_my_theme/functions.php
@@ -96,8 +96,8 @@ if (!function_exists('add_scripts')) { // если ф-я уже есть в до
 	function add_scripts() { // добавление скриптов
 	    if(is_admin()) return false; // если мы в админке - ничего не делаем
 	    wp_deregister_script('jquery'); // выключаем стандартный jquery
-	    wp_enqueue_script('jquery','//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js','','',true); // добавляем свой
-	    wp_enqueue_script('main', get_template_directory_uri().'/js/main.js','','',true); // и скрипты шаблона
+	    wp_enqueue_script('jquery','https://code.jquery.com/jquery-2.2.0.min.js','','',true); // добавляем свой jq
+	    wp_enqueue_script('main', get_template_directory_uri().'/assets/js/main.js','','',true); // и скрипты шаблона
 	}
 }
 
@@ -105,11 +105,11 @@ add_action('wp_print_styles', 'add_styles'); // приклеем ф-ю на до
 if (!function_exists('add_styles')) { // если ф-я уже есть в дочерней теме - нам не надо её определять
 	function add_styles() { // добавление стилей
 	    if(is_admin()) return false; // если мы в админке - ничего не делаем
-		wp_enqueue_style( 'main_reset', get_template_directory_uri().'/css/reset.css' ); 
-		wp_enqueue_style( 'main_fonts', get_template_directory_uri().'/css/fonts.css' ); 
-		wp_enqueue_style( 'main_style_wp', get_template_directory_uri().'/css/style_wp.css' ); 
+		wp_enqueue_style( 'main_reset', get_template_directory_uri().'/assets/css/reset.css' );
+		wp_enqueue_style( 'main_fonts', get_template_directory_uri().'/assets/css/fonts.css' );
+		wp_enqueue_style( 'main_style_wp', get_template_directory_uri().'/assets/css/style_wp.css' );
 		wp_enqueue_style( 'main', get_template_directory_uri().'/style.css' ); 
-		wp_enqueue_style( 'main_media', get_template_directory_uri().'/css/media.css' ); 
+		wp_enqueue_style( 'main_media', get_template_directory_uri().'/assets/css/media.css' );
 	}
 }
 
