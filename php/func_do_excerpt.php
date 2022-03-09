aa498038b1fcdc2a1748ecfd06ce87351de7db52
i.pisarenko <i.pisarenko@crmguru.ru>
2022-03-09 12:19:43 +0300

    сделал возврат значения, а не вывод

--- php/func_do_excerpt.php
+++ php/func_do_excerpt.php
@@ -4,7 +4,7 @@
 function do_excerpt($string, $word_limit) {
 	$words = explode(' ', $string, ($word_limit + 1));
 	if (count($words) > $word_limit) array_pop($words);
-	echo implode(' ', $words) . ' ...';
+	return implode(' ', $words) . ' ...';
 }
 
 ?>
