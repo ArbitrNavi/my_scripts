f39fa31e4c08e183778af172216be617e181db0d
i.pisarenko <i.pisarenko@crmguru.ru>
2022-03-09 11:43:46 +0300

    Обрезать содержимое по словам

---
+++ php/func_do_excerpt.php
@@ -0,0 +1,10 @@
+<?php
+//Обрезать содержимое по словам
+
+function do_excerpt($string, $word_limit) {
+	$words = explode(' ', $string, ($word_limit + 1));
+	if (count($words) > $word_limit) array_pop($words);
+	echo implode(' ', $words) . ' ...';
+}
+
+?>
