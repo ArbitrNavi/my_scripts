3d97d46d120f83d7d4563b160645ed3c7232f4ec
i.pisarenko <i.pisarenko@crmguru.ru>
2022-04-04 11:56:31 +0300

    Ajax подгузка видео роликов v3.12

--- wp/plugins/ajax_youtube/includes/youtube/shortcode_theme_video_shablon.php
+++ wp/plugins/ajax_youtube/includes/youtube/shortcode_theme_video_shablon.php
@@ -2,13 +2,13 @@
 // функция подставки id видео в шорткод из страницы опций acf
 
 // [theme_video_shablon metka="reklama"]
-function theme_video_shortkod($attrs) {
+function func_theme_video_shablon($attrs) {
 	//Вставляем указанную пропорцию видео
-	if (isset($attrs) && array_key_exists('size', $attrs)) {
-		$size = " size=\"" . $attrs['size'] . "\"";
-	} else {
-		$size = "";
-	}
+	//	if (isset($attrs) && array_key_exists('size', $attrs)) {
+	//		$size = " size=\"" . $attrs['size'] . "\"";
+	//	} else {
+	//		$size = "";
+	//	}
 
 	$id_video = [];//массив с id роликов
 	// проверяем есть ли в повторителе данные
@@ -20,7 +20,6 @@ function theme_video_shortkod($attrs) {
 			$metka_field = get_sub_field('metka');//id шорткода
 			$all_video = get_sub_field('all_video');//повторитель со сборником видео
 
-			$metka_arr[] = $metka_field;
 			if ($attrs['metka'] == $metka_field) { //совпадение с меткой
 
 				// видео внутри
@@ -31,20 +30,26 @@ function theme_video_shortkod($attrs) {
 
 				$id_video_str = implode(",", $id_video);//преобразуем массив в строку и разделяем запятой
 
-			};//.совпадение с меткой
+			}//.совпадение с меткой
 
+			$attr_for_shortcode = '';
+			foreach ($attrs as $index => $attr) {
+				if ($index !== 'id' && $index !== 'ids' && $index !== 'metka') {
+					$attr_for_shortcode .= $index . '="' . $attr . '" ';
+				}
+			}
 		endwhile;
 	endif;
 
 	if (count($id_video) == 1) {
-		return (do_shortcode('[theme_video_resp id="' . $id_video_str . '" . ' . $size . ']'));
+		return (do_shortcode('[theme_video_resp id="' . $id_video_str . '" . ' . $attr_for_shortcode . ']'));
 	} else {
-		return (do_shortcode('[theme_video ids="' . $id_video_str . '" . ' . $size . ']'));
+		return (do_shortcode('[theme_video ids="' . $id_video_str . '" . ' . $attr_for_shortcode . ']'));
 	}
 }
 
-//theme_video_shortkod
+//func_theme_video_shablon
 
-add_shortcode('theme_video_shablon', 'theme_video_shortkod');
+add_shortcode('theme_video_shablon', 'func_theme_video_shablon');
 
 ?>
