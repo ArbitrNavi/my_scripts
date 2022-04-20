220be6f864914bd580e891f9c538dab22b151c1b
i.pisarenko <i.pisarenko@crmguru.ru>
2022-04-20 08:44:41 +0300

    адаптация таблицы

--- wp/tablepress_fix.php
+++ wp/tablepress_fix.php
@@ -22,4 +22,20 @@
 		text-indent: 0;
 	}
 
+
+	/*адаптация таблицы*/
+	/*добавить закругления в других ячеек */
+	.tablepress-id-1 .column-3,
+	.tablepress-id-1 .column-4 {
+		display: none;
+	}
+
+	.tablepress-id-1 thead tr th.column-2 {
+		border-radius: 0 20px 0 0 !important;
+	}
+
+	.tablepress tbody tr:last-child .column-2 {
+		border-radius: 0 0 10px 0;
+	}
+
 </style>
