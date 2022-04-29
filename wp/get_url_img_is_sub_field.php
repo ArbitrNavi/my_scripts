fd825f58f23b1eafa47c536c6e496d44c958db49
i.pisarenko <i.pisarenko@crmguru.ru>
2022-04-29 08:36:25 +0300

    fix comments

--- wp/get_url_img_is_sub_field.php
+++ wp/get_url_img_is_sub_field.php
@@ -1,10 +1,9 @@
 <?php
 //Получение url картинки из произвольного поля в повторителе по id картинки
-//$name_field_image_id - имя произвольного поля в повторителе,
-//$img_size - размер для получения
-//$img_default - изображение по умолчанию
-
 function get_url_img_is_sub_field($name_field_image_id, $img_size = 'full', $img_default = '') {
+	//$name_field_image_id - имя произвольного поля в повторителе,
+	//$img_size - размер для получения
+	//$img_default - изображение по умолчанию
 
 	if (get_sub_field($name_field_image_id)) {
 		$resoult = wp_get_attachment_image_url(get_sub_field($name_field_image_id), $img_size);
