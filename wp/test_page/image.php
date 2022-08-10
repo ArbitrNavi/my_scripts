91a0f901c6dbd7a1a7a41c744c25659180d808b3
i.pisarenko <i.pisarenko@crmguru.ru>
2022-08-10 13:51:26 +0300

    тест картинок

---
+++ wp/test_page/image.php
@@ -0,0 +1,56 @@
+тест картинок
+<div>
+
+	[caption id="attachment_16996" align="alignnone" width="1920"]<a href="http://tz-sound-origin.loc/wp-content/uploads/2020/10/17.jpg"><img class="size-full wp-image-16996" src="http://tz-sound-origin.loc/wp-content/uploads/2020/10/17.jpg" alt="" width="1920" height="1280" /></a> без выравнивания большая картинка[/caption]
+
+</div>
+<div>
+
+	[caption id="attachment_16997" align="alignright" width="300"]<a href="http://tz-sound-origin.loc/wp-content/uploads/2020/10/18.jpg"><img class="wp-image-16997 size-medium" src="http://tz-sound-origin.loc/wp-content/uploads/2020/10/18-300x200.jpg" alt="" width="300" height="200" /></a> по правому краю[/caption]
+
+</div>
+<div>
+
+	[caption id="attachment_16979" align="alignleft" width="300"]<a href="http://tz-sound-origin.loc/wp-content/uploads/2020/10/10.jpg"><img class="wp-image-16979 size-medium" src="http://tz-sound-origin.loc/wp-content/uploads/2020/10/10-300x169.jpg" alt="" width="300" height="169" /></a> по левому краю[/caption]
+
+</div>
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+&nbsp;
+
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum, ullam sint labore nobis, accusantium eveniet quod accusamus ad magnam ab cupiditate officiis dolor quaerat mollitia quam quia cum libero. Eum esse fugiat beatae illum quaerat suscipit repellendus, assumenda voluptatum dolorum, vel iste et, praesentium voluptas consequatur velit dolorem consectetur voluptatem perspiciatis voluptates. Magni incidunt iusto ipsam quis a est! Sed, expedita! Expedita, id quidem labore neque, rem, fugit nostrum vel quod possimus sequi inventore saepe, quam ea voluptate architecto fuga modi ipsum unde reiciendis quia alias. Laborum minima, suscipit sed repellendus optio vitae reprehenderit sunt omnis iure modi quas ipsum.
+<div>
+
+	[caption id="attachment_17061" align="aligncenter" width="300"]<a href="http://tz-sound-origin.loc/wp-content/uploads/2020/10/35.jpg"><img class="size-medium wp-image-17061" src="http://tz-sound-origin.loc/wp-content/uploads/2020/10/35-300x169.jpg" alt="Песня под ключ удаленно" width="300" height="169" /></a> По центру[/caption]
+
+</div>
+<div>
+
+	[caption id="attachment_17084" align="alignnone" width="300"]<a href="http://tz-sound-origin.loc/wp-content/uploads/2020/10/46.jpg"><img class="size-medium wp-image-17084" src="http://tz-sound-origin.loc/wp-content/uploads/2020/10/46-300x200.jpg" alt="Создание аудиокниг удаленно" width="300" height="200" /></a> Без выравнивания[/caption]
+
+</div>
+[su_custom_gallery source="media: 4569,17052" link="lightbox" target="blank" width="280" height="280"]
+
+[su_custom_gallery source="media: 17055,17052" link="lightbox" target="blank" width="220" height="180"]
+
+&nbsp;
+<h2>размер изображения на всю ширину</h2>
+&nbsp;
+
+<a href="http://tz-sound-origin.loc/wp-content/uploads/2022/03/452116-beautiful-fantasy-city-wallpaper-1920x1080-1.jpg"><img class="alignnone size-full wp-image-18276" src="http://tz-sound-origin.loc/wp-content/uploads/2022/03/452116-beautiful-fantasy-city-wallpaper-1920x1080-1.jpg" alt="" width="1920" height="1080" /></a>
+
+&nbsp;
+
+&nbsp;
+
+[caption id="attachment_18276" align="alignnone" width="750"]<a href="http://tz-sound-origin.loc/wp-content/uploads/2022/03/452116-beautiful-fantasy-city-wallpaper-1920x1080-1.jpg"><img class="wp-image-18276 size-content" title="Атрибут title" src="http://tz-sound-origin.loc/wp-content/uploads/2022/03/452116-beautiful-fantasy-city-wallpaper-1920x1080-1-750x422.jpg" alt="Атрибут alt" width="750" height="422" /></a> Подпись[/caption]
