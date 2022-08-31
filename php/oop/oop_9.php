d49fa0d25a8dbb914ae8baef7463c10e7b846fb9
i.pisarenko <i.pisarenko@crmguru.ru>
2022-08-31 10:39:30 +0300

    Абстрактный класс и Полиморфизм

--- php/oop/oop_9.php
+++ php/oop/oop_9.php
@@ -5,6 +5,8 @@
 // Минимум абстрактный метод
 // Дочерние классы обязаны реализовать абстрактный метод
 
+// Полиморфизм
+
 abstract class Animal
 {
 	public abstract function makeSound();
@@ -25,10 +27,19 @@ class Dog extends Animal
 	}
 }
 
+class ХозяинЖивотных{
+	public function проситьПодатьЗвук(Animal  $animal){
+		echo "Хозяин животных - ";
+		return $animal->makeSound();
+	}
+}
+
+
 $cat = new Cat;
 $dog = new Dog;
 
-$cat->makeSound();
 $dog->makeSound();
 
-//$animal = new Animal;
+$я = new ХозяинЖивотных;
+$я->проситьПодатьЗвук($cat);
+$я->проситьПодатьЗвук($dog);
