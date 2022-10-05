930bba400983d5f17b4796ad060344092c042c3b
i.pisarenko <i.pisarenko@crmguru.ru>
2022-10-05 22:08:20 +0300

    add func getIdCat, getIdCatPost

--- wp/helpers.php
+++ wp/helpers.php
@@ -1,42 +1,42 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly
 }
 
 
-if ( ! function_exists( 'get_pr' ) ) {
+if (!function_exists('get_pr')) {
 	/**
 	 * Debug function print_r
 	 *
 	 * @param mixed $var
 	 * @param boolean $die
 	 */
-	function get_pr( $var, $die = true ) {
+	function get_pr($var, $die = true) {
 		echo '<pre>';
-		print_r( $var );
+		print_r($var);
 		echo '</pre>';
-		if ( $die ) {
+		if ($die) {
 			die();
 		}
 	}
 }
-if ( ! function_exists( 'get_vd' ) ) {
+if (!function_exists('get_vd')) {
 	/**
 	 * Debug function var_dump
 	 *
 	 * @param mixed $var
 	 * @param boolean $die
 	 */
-	function get_vd( $var, $die = true ) {
+	function get_vd($var, $die = true) {
 		echo '<pre>';
-		var_dump( $var );
+		var_dump($var);
 		echo '</pre>';
-		if ( $die ) {
+		if ($die) {
 			die();
 		}
 	}
 }
-if ( ! function_exists( 'get_num_ending' ) ) {
+if (!function_exists('get_num_ending')) {
 	/**
 	 * Склонения числительных
 	 *
@@ -45,33 +45,33 @@ if ( ! function_exists( 'get_num_ending' ) ) {
 	 *
 	 * @return mixed
 	 */
-	function get_num_ending( $number, $ending_array ) {
+	function get_num_ending($number, $ending_array) {
 		$number = $number % 100;
-		if ( $number >= 11 && $number <= 19 ) {
+		if ($number >= 11 && $number <= 19) {
 			$ending = $ending_array[2];
 		} else {
 			$i = $number % 10;
-			switch ( $i ) {
-				case ( 1 ):
+			switch ($i) {
+				case (1):
 					$ending = $ending_array[0];
 					break;
-				case ( 2 ):
-				case ( 3 ):
-				case ( 4 ):
+				case (2):
+				case (3):
+				case (4):
 					$ending = $ending_array[1];
 					break;
 				default:
 					$ending = $ending_array[2];
 			}
 		}
-		
+
 		return $ending;
 	}
 }
 
 
-function js_log($data){
-	echo "<script>console.log('". $data ."');</script>";
+function js_log($data) {
+	echo "<script>console.log('" . $data . "');</script>";
 }
 
 
@@ -81,4 +81,20 @@ function phone_format($phone_string) { //' +7 (905) 506-3-506'
 	$phone = preg_replace($pattern, $replacement, $phone_string);
 	$resoult = (substr($phone, 0, 1) == "7") ? "+" . $phone : $phone;
 	return $resoult; //89055063506 || +79055063506
+}
+
+
+function getIdCat() {
+	// id текущей категории
+	$queried_object = get_queried_object();
+	$taxonomy = $queried_object->taxonomy;
+	$term_id = $queried_object->term_id;
+	return $term_id;
+}
+
+function getIdCatPost() {
+	// id категории к которой принадлежит текущая запись
+	$infocat = get_the_category();
+	$catID = $infocat[0]->cat_ID;
+	return $catID;
 }
