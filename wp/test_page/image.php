ef9b39d7599b0a5d03d8b8c62f61ff67fb9caf67
i.pisarenko <i.pisarenko@crmguru.ru>
2022-11-02 07:40:03 +0300

    add tags

--- wp/test_page/image.php
+++ wp/test_page/image.php
@@ -1,25 +1,51 @@
 тест картинок
 <div>
-	[caption id="attachment_16996" align="alignnone" width="1920"]<a href="http://tz-sound-origin.loc/wp-content/uploads/2020/10/17.jpg"><img class="size-full wp-image-16996" src="http://tz-sound-origin.loc/wp-content/uploads/2020/10/17.jpg" alt="" width="1920" height="1280"/></a> без выравнивания большая картинка[/caption]
+	[caption id="attachment_16996" align="alignnone" width="1920"]<a href="https://tz-sound.ru/wp-content/uploads/2020/10/17.jpg"><img class="size-full wp-image-16996" src="https://tz-sound.ru/wp-content/uploads/2020/10/17.jpg" alt="" width="1920" height="1280"/></a> без выравнивания большая картинка[/caption]
 </div>
 <div>
-	[caption id="attachment_16997" align="alignright" width="300"]<a href="http://tz-sound-origin.loc/wp-content/uploads/2020/10/18.jpg"><img class="wp-image-16997 size-medium" src="http://tz-sound-origin.loc/wp-content/uploads/2020/10/18-300x200.jpg" alt="" width="300" height="200"/></a> по правому краю[/caption]
+	[caption id="attachment_16997" align="alignright" width="300"]<a href="https://tz-sound.ru/wp-content/uploads/2020/10/18.jpg"><img class="wp-image-16997 size-medium" src="https://tz-sound.ru/wp-content/uploads/2020/10/18-300x200.jpg" alt="" width="300" height="200"/></a> по правому краю[/caption]
 </div>
 <div>
-	[caption id="attachment_16979" align="alignleft" width="300"]<a href="http://tz-sound-origin.loc/wp-content/uploads/2020/10/10.jpg"><img class="wp-image-16979 size-medium" src="http://tz-sound-origin.loc/wp-content/uploads/2020/10/10-300x169.jpg" alt="" width="300" height="169"/></a> по левому краю[/caption]
+	[caption id="attachment_16979" align="alignleft" width="300"]<a href="https://tz-sound.ru/wp-content/uploads/2020/10/10.jpg"><img class="wp-image-16979 size-medium" src="https://tz-sound.ru/wp-content/uploads/2020/10/10-300x169.jpg" alt="" width="300" height="169"/></a> по левому краю[/caption]
 </div>
 
 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum, ullam sint labore nobis, accusantium eveniet quod accusamus ad magnam ab cupiditate officiis dolor quaerat mollitia quam quia cum libero. Eum esse fugiat beatae illum quaerat suscipit repellendus, assumenda voluptatum dolorum, vel iste et, praesentium voluptas consequatur velit dolorem consectetur voluptatem perspiciatis voluptates. Magni incidunt iusto ipsam quis a est! Sed, expedita! Expedita, id quidem labore neque, rem, fugit nostrum vel quod possimus sequi inventore saepe, quam ea voluptate architecto fuga modi ipsum unde reiciendis quia alias. Laborum minima, suscipit sed repellendus optio vitae reprehenderit sunt omnis iure modi quas ipsum.
 
 <div>
-	[caption id="attachment_17061" align="aligncenter" width="300"]<a href="http://tz-sound-origin.loc/wp-content/uploads/2020/10/35.jpg"><img class="size-medium wp-image-17061" src="http://tz-sound-origin.loc/wp-content/uploads/2020/10/35-300x169.jpg" alt="Песня под ключ удаленно" width="300" height="169"/></a> По центру[/caption]
+	[caption id="attachment_17061" align="aligncenter" width="300"]<a href="https://tz-sound.ru/wp-content/uploads/2020/10/35.jpg"><img class="size-medium wp-image-17061" src="https://tz-sound.ru/wp-content/uploads/2020/10/35-300x169.jpg" alt="Песня под ключ удаленно" width="300" height="169"/></a> По центру[/caption]
 </div>
 <div>
-	[caption id="attachment_17084" align="alignnone" width="300"]<a href="http://tz-sound-origin.loc/wp-content/uploads/2020/10/46.jpg"><img class="size-medium wp-image-17084" src="http://tz-sound-origin.loc/wp-content/uploads/2020/10/46-300x200.jpg" alt="Создание аудиокниг удаленно" width="300" height="200"/></a> Без выравнивания[/caption]
-</div>
-[su_custom_gallery source="media: 4569,17052" link="lightbox" target="blank" width="280" height="280"]
-[su_custom_gallery source="media: 17055,17052" link="lightbox" target="blank" width="220" height="180"]
+	[caption id="attachment_17084" align="alignnone" width="300"]<a href="https://tz-sound.ru/wp-content/uploads/2020/10/46.jpg"><img class="size-medium wp-image-17084" src="https://tz-sound.ru/wp-content/uploads/2020/10/46-300x200.jpg" alt="Создание аудиокниг удаленно" width="300" height="200"/></a> Без выравнивания[/caption]
+</div>[su_custom_gallery source="media: 4569,17052" link="lightbox" target="blank" width="280" height="280"][su_custom_gallery source="media: 17055,17052" link="lightbox" target="blank" width="220" height="180"]
 <h2>размер изображения на всю ширину</h2>
-<a href="http://tz-sound-origin.loc/wp-content/uploads/2022/03/452116-beautiful-fantasy-city-wallpaper-1920x1080-1.jpg"><img class="alignnone size-full wp-image-18276" src="http://tz-sound-origin.loc/wp-content/uploads/2022/03/452116-beautiful-fantasy-city-wallpaper-1920x1080-1.jpg" alt="" width="1920" height="1080"/></a>
-[caption id="attachment_18276" align="alignnone" width="750"]
-<a href="http://tz-sound-origin.loc/wp-content/uploads/2022/03/452116-beautiful-fantasy-city-wallpaper-1920x1080-1.jpg"><img class="wp-image-18276 size-content" title="Атрибут title" src="http://tz-sound-origin.loc/wp-content/uploads/2022/03/452116-beautiful-fantasy-city-wallpaper-1920x1080-1-750x422.jpg" alt="Атрибут alt" width="750" height="422"/></a> Подпись[/caption]
+<a href="https://tz-sound.ru/wp-content/uploads/2022/03/452116-beautiful-fantasy-city-wallpaper-1920x1080-1.jpg"><img class="alignnone size-full wp-image-18276" src="https://tz-sound.ru/wp-content/uploads/2022/03/452116-beautiful-fantasy-city-wallpaper-1920x1080-1.jpg" alt="" width="1920" height="1080"/></a>[caption id="attachment_18276" align="alignnone" width="750"]
+<a href="https://tz-sound.ru/wp-content/uploads/2022/03/452116-beautiful-fantasy-city-wallpaper-1920x1080-1.jpg"><img class="wp-image-18276 size-content" title="Атрибут title" src="https://tz-sound.ru/wp-content/uploads/2022/03/452116-beautiful-fantasy-city-wallpaper-1920x1080-1-750x422.jpg" alt="Атрибут alt" width="750" height="422"/></a> Подпись[/caption]
+
+<h1>Заголовок h1</h1><h2>Заголовок h2</h2><h3>Заголовок h3</h3><h4>Заголовок h4</h4><h5>Заголовок h5</h5>
+<h6>Заголовок h6</h6>
+
+<p>Параграф любой. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus amet autem exercitationem fugiat iusto laboriosam quis, recusandae reiciendis tempora velit.</p>
+<span>span Lorem ipsum dolor sit.</span>
+
+<a href="#">Ссылка Lorem ipsum.</a><p><strong>strong Lorem ipsum dolor.</strong></p><p><em>em Lorem ipsum dolor.</em>
+</p><p><strong><em>strong em Lorem ipsum dolor.</em></strong></p><p style="text-align: center;">align center</p>
+<p style="text-align: right;">align right</p>
+
+<blockquote>
+	<p>цитата любая Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, unde?</p>
+</blockquote>
+
+<ul>
+	<li>Маркерованный список</li>
+	<li>маркер</li>
+	<li>маркер</li>
+	<li>маркер</li>
+</ul>
+
+<ol>
+	<li>нумерованный список</li>
+	<li>нумерованный</li>
+	<li>нумерованный</li>
+	<li>нумерованный</li>
+	<li>нумерованный</li>
+</ol>
