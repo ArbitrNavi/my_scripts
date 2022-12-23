2cae44e8280d839e2a77964ea145c08cf1fc0925
Ivan <it-1ivan@ya.ru>
2022-12-23 16:08:07 +0300

    add getPersonalizaciyaStyleBg

--- wp/helpers.php
+++ wp/helpers.php
@@ -122,3 +122,28 @@ function myRand( $length = 10 ) {
 
 	return $stringRand;
 }
+
+function getPersonalizaciyaStyleBg($fieldCat = false, $fieldOptions = false, $dataDefault = false, $visibleFrontPage = true) {
+	//$fieldCat - поле в рубриках
+	//$fieldOptions - поле на странице опций
+	//$dataDefault - путь к картинке по умолчанию
+	//$visibleFrontPage - показывать на главной или нет
+	if (is_front_page() && !$visibleFrontPage) {//скрыть на главной этот стиль
+		$bg_faces = null;
+	} else {//какой стиль показываем
+		if (getField($fieldCat, true) && !is_front_page()) {//изображение берется из рубрики или родительской рубрики текущей записи
+			$faceBgImgId = getField($fieldCat, true);
+			$bg_faces_url = wp_get_attachment_image_url($faceBgImgId, 'full');
+		} elseif (get_field($fieldOptions, 'options')) {//изображение со страницы настроек темы
+			$bg_faces_url = wp_get_attachment_image_url(get_field($fieldOptions, 'options'), 'full');
+		} elseif ($dataDefault) {//изображение по умолчанию
+			$bg_faces_url = $dataDefault;
+		} else {// если никакие условия не подходит, ни чего не возвращаем
+			$bg_faces_url = null;
+		}
+
+		$bg_faces = ($bg_faces_url) ? "background: url(\"" . $bg_faces_url . "\") center top / cover;" : null;
+	}
+
+	return $bg_faces;
+}
