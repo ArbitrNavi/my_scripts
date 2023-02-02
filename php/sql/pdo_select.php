321cfb70ace277b34938718d562efe2b166e3f50
Ivan <it-1ivan@ya.ru>
2023-02-02 16:44:39 +0300

    insert var for SQL

--- php/sql/pdo_select.php
+++ php/sql/pdo_select.php
@@ -7,12 +7,12 @@ $statement = $pdo->prepare("SELECT * FROM my_table"); //ЗАПРОС SELECT
 
 //$users = $statement->fetchAll(PDO::FETCH_ASSOC); //получить ответ от бд
 
-
+$nameCol2 = "email";
 // получить пользователя с id 3
-$statement = $pdo->prepare("SELECT id FROM my_table WHERE id=:nomer");//шаблон запроса
+$statement = $pdo->prepare("SELECT {$nameCol2} FROM my_table WHERE id=:nomer");//шаблон запроса
 //добавить переменные
 $statement->execute([
-	"nomer"=>5,
+	"nomer"=>12,
 //	"key1" =>'id'
 ]);
 
