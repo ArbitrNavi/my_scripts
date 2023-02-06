7c70a105b8ba5d1f3764fe8ba8a19345cb0d6917
Ivan <it-1ivan@ya.ru>
2023-02-07 00:49:35 +0300

    formatting code

--- php/marlin/registration/users.php
+++ php/marlin/registration/users.php
@@ -53,7 +53,6 @@
 			if (isAdmin()) { ?>
 				<a class="btn btn-success" href="create_user.php">Добавить</a>
 			<?php } ?>
-
 			<div class="border-faded bg-faded p-3 mb-g d-flex mt-3">
 				<input type="text" id="js-filter-contacts" name="filter-contacts" class="form-control shadow-inset-2 form-control-lg" placeholder="Найти пользователя">
 				<div class="btn-group btn-group-lg btn-group-toggle hidden-lg-down ml-3" data-toggle="buttons">
