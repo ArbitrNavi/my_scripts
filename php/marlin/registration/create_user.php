b91adb2cfc25ae79361ab6340c27ae71302ccf7b
Ivan <it-1ivan@ya.ru>
2023-02-07 00:49:14 +0300

    add check is login

--- php/marlin/registration/create_user.php
+++ php/marlin/registration/create_user.php
@@ -1,3 +1,10 @@
+<?php session_start();
+require_once "php/functions.php";
+if (is_not_login() || !isAdmin()) {
+	redirect("page_login.php");
+	exit();
+};
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,7 +22,9 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary bg-primary-gradient">
-	<a class="navbar-brand d-flex align-items-center fw-500" href="users.html"><img alt="logo" class="d-inline-block align-top mr-2" src="img/logo.png"> Учебный проект</a> <button aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarColor02" data-toggle="collapse" type="button"><span class="navbar-toggler-icon"></span></button>
+	<a class="navbar-brand d-flex align-items-center fw-500" href="users.html"><img alt="logo" class="d-inline-block align-top mr-2" src="img/logo.png"> Учебный проект</a>
+	<button aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarColor02" data-toggle="collapse" type="button">
+		<span class="navbar-toggler-icon"></span></button>
 	<div class="collapse navbar-collapse" id="navbarColor02">
 		<ul class="navbar-nav mr-auto">
 			<li class="nav-item active">
@@ -35,9 +44,7 @@
 <main id="js-page-content" role="main" class="page-content mt-3">
 	<div class="subheader">
 		<h1 class="subheader-title">
-			<i class='subheader-icon fal fa-plus-circle'></i> Добавить пользователя
-		</h1>
-
+			<i class='subheader-icon fal fa-plus-circle'></i> Добавить пользователя </h1>
 
 
 	</div>
@@ -185,11 +192,10 @@
 <script src="js/app.bundle.js"></script>
 <script>
 
-    $(document).ready(function()
-    {
+	$(document).ready(function () {
 
 
-    });
+	});
 
 </script>
 </body>
