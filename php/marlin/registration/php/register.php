4b42124fd28db4402563222b890abbc2801c4942
Ivan <it-1ivan@ya.ru>
2023-02-08 11:33:16 +0300

    optimize code

--- php/marlin/registration/php/register.php
+++ php/marlin/registration/php/register.php
@@ -1,6 +1,7 @@
 <?php session_start();
 include_once "functions.php";
-if ( arrayValue($_POST, "email") === 'null' || arrayValue($_POST, 'password') === 'null') {
+
+if (arrayValue($_POST, "email") === 'null' || arrayValue($_POST, 'password') === 'null') {
 	redirect('../page_register.php');
 	die();
 } else {
@@ -8,30 +9,15 @@ if ( arrayValue($_POST, "email") === 'null' || arrayValue($_POST, 'password') ==
 	$password = $_POST["password"];
 }
 
-$sqlDataWhere = [
-	"email" => $email,
-];
-
-$pdo = connectBD();
-
-$statement = $pdo->prepare("SELECT * FROM users WHERE email = :email"); //ЗАПРОС SELECT
-//$statement = $pdo->prepare("SELECT * FROM users WHERE email = :email AND password = :password;"); //ЗАПРОС SELECT
-$statement->execute($sqlDataWhere); //ПОЛУЧИТЬ РЕЗУЛЬТАТ
-$users = $statement->fetchAll(PDO::FETCH_ASSOC); //ПЕРЕДАЕМ ДАННЫЕ В ПЕРЕМЕННУЮ USER
-
-
-if ($users) {
+if (add_user($email, $password)) {
 	$_SESSION["is_create_user"] = false;
 
 	redirect('../page_register.php');
 } else {
-	add_user($email, $password);
-
 	$_SESSION["is_create_user"] = true;
 
 	redirect('../page_login.php');
 }
-
 
 
 //echo "<pre>";
