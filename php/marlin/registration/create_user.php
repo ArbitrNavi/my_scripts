2b5d922afa975d4437ae483259990868d380722a
Ivan <it-1ivan@ya.ru>
2023-02-08 15:44:18 +0300

    fix actions php

--- php/marlin/registration/create_user.php
+++ php/marlin/registration/create_user.php
@@ -46,7 +46,7 @@ if (!isAdmin()) {
 		<h1 class="subheader-title">
 			<i class='subheader-icon fal fa-plus-circle'></i> Добавить пользователя </h1>
 	</div>
-	<form action="">
+	<form action="php/addUserAddData.php" method="POST">
 		<div class="row">
 			<div class="col-xl-6">
 				<div id="panel-1" class="panel">
@@ -57,26 +57,26 @@ if (!isAdmin()) {
 						<div class="panel-content">
 							<!-- username -->
 							<div class="form-group">
-								<label class="form-label" for="simpleinput"></label>
-								<input type="text" id="simpleinput" class="form-control" value="">
+								<label class="form-label" for="simpleinput">Имя</label>
+								<input type="text" id="simpleinput" class="form-control" value="" name="name">
 							</div>
 
 							<!-- title -->
 							<div class="form-group">
 								<label class="form-label" for="simpleinput">Место работы</label>
-								<input type="text" id="simpleinput" class="form-control">
+								<input type="text" id="simpleinput" class="form-control" name="works">
 							</div>
 
 							<!-- tel -->
 							<div class="form-group">
 								<label class="form-label" for="simpleinput">Номер телефона</label>
-								<input type="text" id="simpleinput" class="form-control">
+								<input type="text" id="simpleinput" class="form-control" name="phone">
 							</div>
 
 							<!-- address -->
 							<div class="form-group">
 								<label class="form-label" for="simpleinput">Адрес</label>
-								<input type="text" id="simpleinput" class="form-control">
+								<input type="text" id="simpleinput" class="form-control" name="address">
 							</div>
 						</div>
 					</div>
@@ -93,20 +93,20 @@ if (!isAdmin()) {
 							<!-- email -->
 							<div class="form-group">
 								<label class="form-label" for="simpleinput">Email</label>
-								<input type="text" id="simpleinput" class="form-control" value="">
+								<input type="email" id="simpleinput" class="form-control" value="" name="email">
 							</div>
 
 							<!-- password -->
 							<div class="form-group">
 								<label class="form-label" for="simpleinput">Пароль</label>
-								<input type="password" id="simpleinput" class="form-control">
+								<input type="password" id="simpleinput" class="form-control" name="password">
 							</div>
 
 
 							<!-- status -->
 							<div class="form-group">
 								<label class="form-label" for="example-select">Выберите статус</label>
-								<select class="form-control" id="example-select">
+								<select class="form-control" id="example-select" name="status">
 									<option>Онлайн</option>
 									<option>Отошел</option>
 									<option>Не беспокоить</option>
@@ -115,7 +115,7 @@ if (!isAdmin()) {
 
 							<div class="form-group">
 								<label class="form-label" for="example-fileinput">Загрузить аватар</label>
-								<input type="file" id="example-fileinput" class="form-control-file">
+								<input type="file" id="example-fileinput" class="form-control-file" name="avatar">
 							</div>
 						</div>
 					</div>
@@ -142,7 +142,7 @@ if (!isAdmin()) {
                                                     </span>
                                                 </span>
 										</div>
-										<input type="text" class="form-control border-left-0 bg-transparent pl-0">
+										<input type="text" class="form-control border-left-0 bg-transparent pl-0" name="vk">
 									</div>
 								</div>
 								<div class="col-md-4">
@@ -156,7 +156,7 @@ if (!isAdmin()) {
                                                     </span>
                                                 </span>
 										</div>
-										<input type="text" class="form-control border-left-0 bg-transparent pl-0">
+										<input type="text" class="form-control border-left-0 bg-transparent pl-0" name="telegram">
 									</div>
 								</div>
 								<div class="col-md-4">
@@ -170,7 +170,7 @@ if (!isAdmin()) {
                                                     </span>
                                                 </span>
 										</div>
-										<input type="text" class="form-control border-left-0 bg-transparent pl-0">
+										<input type="text" class="form-control border-left-0 bg-transparent pl-0" name="instagram">
 									</div>
 								</div>
 								<div class="col-md-12 mt-3 d-flex flex-row-reverse">
