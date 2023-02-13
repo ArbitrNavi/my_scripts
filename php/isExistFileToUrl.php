e20772b6e409735c49732970d7f4340c37a26365
Ivan <it-1ivan@ya.ru>
2023-02-13 12:03:23 +0300

    fix check

--- php/isExistFileToUrl.php
+++ php/isExistFileToUrl.php
@@ -1,13 +1,15 @@
 <?php
-function UR_exists($url){
-	$headers=get_headers($url);
-	return stripos($headers[0],"200 OK")?true:false;
+function UR_exists($url) {
+	if ($url) {
+		$headers = get_headers($url);
+		return stripos($headers[0], "200 OK") ? true : false;
+	}
 }
 
 $filename = 'http://realty.loc/wp-content/webp-express/webp-images/uploads/2023/01/Frame-12.png.webp';
 /* You can test a URL like this (sample) */
-if(UR_exists($filename)){
-	echo "This page exists";}
-else {
+if (UR_exists($filename)) {
+	echo "This page exists";
+} else {
 	echo "This page does not exist";
 }
