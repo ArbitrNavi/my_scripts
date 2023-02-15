33e4988588dfe922bc1a729b58d3f0f9778cc7e4
Ivan <it-1ivan@ya.ru>
2023-02-15 14:23:45 +0300

    task_4 move getAllPosts to functions.php

--- php/marlin/thinkingOOP/functions.php
+++ php/marlin/thinkingOOP/functions.php
@@ -5,3 +5,13 @@ function dd($var) {
 	echo "</pre>";
 	die();
 }
+
+function getAllPosts() {
+
+	$pdo = new PDO("mysql:host=localhost;dbname=app3;", "root", "");
+
+	$statement = $pdo->prepare("SELECT * FROM posts"); //ЗАПРОС SELECT
+	$statement->execute(); //ПОЛУЧИТЬ РЕЗУЛЬТАТ
+	$posts = $statement->fetchAll(PDO::FETCH_ASSOC); //ПЕРЕДАЕМ ДАННЫЕ В ПЕРЕМЕННУЮ USER
+	return $posts;
+}
--- php/marlin/thinkingOOP/index.php
+++ php/marlin/thinkingOOP/index.php
@@ -1,16 +1,6 @@
 <?php
 include "functions.php";
 
-function getAllPosts() {
-
-	$pdo = new PDO("mysql:host=localhost;dbname=app3;", "root", "");
-
-	$statement = $pdo->prepare("SELECT * FROM posts"); //ЗАПРОС SELECT
-	$statement->execute(); //ПОЛУЧИТЬ РЕЗУЛЬТАТ
-	$posts = $statement->fetchAll(PDO::FETCH_ASSOC); //ПЕРЕДАЕМ ДАННЫЕ В ПЕРЕМЕННУЮ USER
-	return $posts;
-}
-
 $posts = getAllPosts();
 //dd($posts);
 
