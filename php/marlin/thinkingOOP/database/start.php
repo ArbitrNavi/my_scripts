b60c9e2b469d8f05237a53f1428a8540e9e142bf
Ivan <it-1ivan@ya.ru>
2023-02-15 15:53:00 +0300

    task_6 create start.php, move work code of BD to new file

---
+++ php/marlin/thinkingOOP/database/start.php
@@ -0,0 +1,5 @@
+<?php
+include "database/QueryBuilder.php";
+include "database/Connection.php";
+
+return new QueryBuilder(Connection::make());
--- php/marlin/thinkingOOP/index.php
+++ php/marlin/thinkingOOP/index.php
@@ -1,11 +1,7 @@
 <?php
 include "functions.php";
-include "database/QueryBuilder.php";
-include "database/Connection.php";
+$db = include "database/start.php";
 
-$pdo = Connection::make();
-
-$db = new QueryBuilder($pdo);
 $posts = $db->getAllPosts();
 
 include "index.view.php";
