8d5d1cf763760f48c31e198f38db6fc8fc14efba
Ivan <it-1ivan@ya.ru>
2023-02-17 13:09:49 +0300

    add function create()

--- php/marlin/thinkingOOP/database/QueryBuilder.php
+++ php/marlin/thinkingOOP/database/QueryBuilder.php
@@ -14,4 +14,10 @@ class QueryBuilder
 		$statement->execute(); //ПОЛУЧИТЬ РЕЗУЛЬТАТ
 		return $statement->fetchAll(PDO::FETCH_ASSOC);
 	}
+
+	public function create() {
+		$sql = "INSERT INTO posts (title) VALUES ('Любой текст из класса')";
+		$statement =  $this->pdo->query($sql);
+		dd($statement);
+	}
 }
