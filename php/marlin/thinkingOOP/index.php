3efe0c55731fc32aa1b590b7e0cb06f0083597f7
Ivan <it-1ivan@ya.ru>
2023-02-20 12:40:04 +0300

    task_12 add $routes

--- php/marlin/thinkingOOP/index.php
+++ php/marlin/thinkingOOP/index.php
@@ -2,5 +2,19 @@
 include "functions.php";
 $db = include "database/start.php";
 $posts = $db->getAll("posts");
-include "index.view.php";
+
+$routes = [
+	"/"      => "functions/homepage.php",
+	"/about" => "functions/about.php",
+];
+
+$route = $_SERVER["REQUEST_URI"];
+
+if (array_key_exists($route, $routes)) {
+	include $routes[$route];
+} else {
+	dd(404);
+}
+
+//include "index.view.php";
 ?>
