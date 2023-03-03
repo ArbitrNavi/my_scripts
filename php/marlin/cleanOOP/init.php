0ba612194bd06e51a33a164aaa10486d3fba1f2a
Ivan <it-1ivan@ya.ru>
2023-03-03 12:15:55 +0300

    cleanOOP_t16 include config, add login form

--- php/marlin/cleanOOP/init.php
+++ php/marlin/cleanOOP/init.php
@@ -26,3 +26,80 @@ $GLOBALS["config"] = [
 		'token_name' => 'token',
 	]
 ];
+
+if (Input::exists()) {
+	if (Token::check(Input::get('token'))) {
+		$validate = new Validate();
+		$validation = $validate->check($_POST, [
+			'username'       => [
+				'required' => true,
+				'min'      => 2,
+				'max'      => 15,
+				'unique'   => 'users'
+			],
+			'email'          => [
+				'required' => true,
+				'email'    => true,
+				'unique'   => 'users'
+			],
+			'password'       => [
+				'required' => true,
+				'min'      => 3,
+			],
+			'password_again' => [
+				'required' => true,
+				'matches'  => 'password'
+			],
+		]);
+
+
+		//	var_dump($validation->errors());
+
+		if ($validation->passed()) {
+
+			$user = new User();
+			$user->create([
+				'username' => Input::get('username'),
+				'email'    => Input::get('email'),
+				'password' => password_hash(Input::get('password'), PASSWORD_DEFAULT)
+			]);
+
+			Session::flash('success', 'user register done');
+			//			header('Location: test.php');
+		} else {
+			foreach ($validation->errors() as $error) {
+				echo $error . '<br>';
+			}
+		}
+	}
+
+}
+
+
+//Database::getInstance()->insert('users', [
+//	'username' => 'Marlin',
+//	'password' => 'pass',
+//]);
+//Database::getInstance()->update('users', 5, [
+//	'username' => 'Marlin2',
+//	'password' => 'pass2',
+//]);
+
+
+
+$users = Database::getInstance()->get('users', ['username', '=', 'Marlin']);
+//Database::getInstance()->delete('users', ['username', '=', 'name2']);
+
+
+//if ($users->error()) {
+//	echo "This Error";
+//} else {
+//	foreach ($users->result() as $user) {
+//		echo $user["id"] . ". " . $user["username"] . "<br>";
+//		//		echo $user . "<br>";
+//	}
+//}
+//Redirect::to(404);
+
+
+//var_dump($_SESSION);
--- php/marlin/cleanOOP/login.php
+++ php/marlin/cleanOOP/login.php
@@ -1,15 +1,6 @@
-<?php
-session_start();
-require_once "init.php";
-?>
-
-
+<?php require_once "init.php"; ?>
 <form action="" method="post">
 	<?php echo Session::flash('success'); ?>
-	<div class="field">
-		<label for="username">Username</label>
-		<input type="text" name="username" value="<?php echo Input::get('username'); ?>">
-	</div>
 	<div class="field">
 		<label for="email">Email</label>
 		<input type="text" name="email" value="<?php echo Input::get('email'); ?>">
@@ -18,14 +9,9 @@ require_once "init.php";
 		<label for="password">Password</label>
 		<input type="text" name="password">
 	</div>
-	<div class="field">
-		<label for="password_again">Password again</label>
-		<input type="text" name="password_again">
-	</div>
 	<input type="hidden1" name="token" value="<?php echo Token::generate() ?>">
 
 	<div class="field">
 		<button type="submit">Submit</button>
 	</div>
 </form>
-
