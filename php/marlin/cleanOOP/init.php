7ef825bbf445bf5daf9d70c4c7daa369ce6c770c
Ivan <it-1ivan@ya.ru>
2023-03-03 11:37:37 +0300

    cleanOOP_t15 move class for catalog Clases

---
+++ php/marlin/cleanOOP/init.php
@@ -0,0 +1,9 @@
+<?php
+include_once "Clases/Config.php";
+include_once "Clases/Database.php";
+include_once "Clases/Validate.php";
+include_once "Clases/Input.php";
+include_once "Clases/Token.php";
+include_once "Clases/Session.php";
+include_once "Clases/User.php";
+include_once "Clases/Redirect.php";
