a672c3bf11de83e55f4d15a56281e3620a1263e8
Ivan <it-1ivan@ya.ru>
2023-03-20 14:57:56 +0300

    first Plates

--- php/marlin/deepOOP/public/index.php
+++ php/marlin/deepOOP/public/index.php
@@ -1,8 +1,15 @@
 <?php
 require_once "../vendor/autoload.php";
 
-if ($_SERVER['REQUEST_URI'] == '/php/marlin/deepOOP/public/home') {
-	require '../app/controlles/homepage.php';
-}
+//if ($_SERVER['REQUEST_URI'] == '/php/marlin/deepOOP/public/home') {
+//	require '../app/controlles/homepage.php';
+//}
 
-exit();
+// Create new Plates instance
+$templates = new League\Plates\Engine('../app/views');
+
+//var_dump($templates);
+//die;
+
+// Render a template
+echo $templates->render('homepage');
