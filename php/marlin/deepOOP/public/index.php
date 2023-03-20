9b32e0e6a7ba1e608c39e7bf31bee76023ebd935
Ivan <it-1ivan@ya.ru>
2023-03-20 16:35:36 +0300

    done t10 fix small error

--- php/marlin/deepOOP/public/index.php
+++ php/marlin/deepOOP/public/index.php
@@ -13,7 +13,7 @@ require_once "../vendor/autoload.php";
 // Create new Plates instance
 $templates = new League\Plates\Engine('../app/views');
 
-flash()->message("Text error", "error");
+flash()->message("Other text", "success");
 flash()->warning("text warning");
 //d($templates);
 // Render a template
