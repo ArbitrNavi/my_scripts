362130adaa33ee5e5b04e2d2909173266d086dfa
Ivan <it-1ivan@ya.ru>
2023-03-20 10:46:03 +0300

    remove trash code

--- php/marlin/deepOOP/app/controlles/homepage.php
+++ php/marlin/deepOOP/app/controlles/homepage.php
@@ -1,25 +1,5 @@
 <?php
 
 use App\QueryBuilder;
-
-//echo "this homepage.php";
-
 $db = new QueryBuilder();
 
-
-echo "<pre>";
-var_dump($db->getAll('posts'));
-
-$data = [
-//	"id"    => 36,
-	"title" => "Новый заголовок update " . date('U')
-];
-
-//$db->update($data, 36, 'posts');
-
-
-$db->delete(48, 'posts');
-//$db->insert($data, 'posts');
-
-echo "<hr>";
-echo "<hr>";
