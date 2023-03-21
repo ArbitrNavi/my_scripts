bc3df242c043321c59bda1b699de13a8e404ce5c
Ivan <it-1ivan@ya.ru>
2023-03-21 17:05:35 +0300

    done t11 add route start

--- php/marlin/deepOOP/public/index.php
+++ php/marlin/deepOOP/public/index.php
@@ -1,8 +1,5 @@
 <?php
-// Start a Session
-if( !session_id() ) {
-	session_start();
-}
+if( !session_id() ) {session_start();}
 
 require_once "../vendor/autoload.php";
 
@@ -11,11 +8,46 @@ require_once "../vendor/autoload.php";
 //}
 
 // Create new Plates instance
-$templates = new League\Plates\Engine('../app/views');
-
-flash()->message("Other text", "success");
-flash()->warning("text warning");
-//d($templates);
-// Render a template
-echo $templates->render('homepage', ['name'=>'Artur']);
+//$templates = new League\Plates\Engine('../app/views');
+//flash()->message("Other text", "success");
+//flash()->warning("text warning");
+//echo $templates->render('homepage', ['name'=>'Artur']);
 //echo $templates->render('about', ['name'=>'Artur']);
+
+d($_SERVER);
+$dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
+	$r->addRoute('GET', '/php/marlin/deepOOP/public/users', 'get_all_users_handler');
+	// {id} must be a number (\d+) //user/1
+	$r->addRoute('GET', '/user/{id:\d+}', 'get_user_handler');
+	// The /{title} suffix is optional
+	$r->addRoute('GET', '/articles/{id:\d+}[/{title}]', 'get_article_handler');
+});
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+	$uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+d($routeInfo);
+
+switch ($routeInfo[0]) {
+	case FastRoute\Dispatcher::NOT_FOUND:
+		// ... 404 Not Found
+		break;
+	case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+		$allowedMethods = $routeInfo[1];
+		// ... 405 Method Not Allowed
+		break;
+	case FastRoute\Dispatcher::FOUND:
+		$handler = $routeInfo[1];
+		$vars = $routeInfo[2];
+		// ... call $handler with $vars
+		break;
+}
